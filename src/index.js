bcca26fff769f1b15ca6c1f9d27f5162aa1d6e02
Mike Hodnick <mike@kindohm.com>
2020-03-19 16:32:19 -0500

    generate event and talk files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const { format } = require('date-fns');
 const events = require('./../json-archive/events');
+const talks = require('./../json-archive/talks');
+
+// delete everything under out
+fs.removeSync('out');
+fs.mkdirSync('out');
+
+// ensure paths
+const eventsContentPath = 'out/events-content';
+const eventsPath = `${eventsContentPath}/_events`;
+const talksPath = `${eventsContentPath}/_talks`;
+
+fs.mkdirSync(eventsContentPath);
+fs.mkdirSync(eventsPath);
+fs.mkdirSync(talksPath);
 
 /*
 for each event, create events-content/_events/tccc##.md file
@@ -39,3 +53,39 @@ eventsFiles.forEach(file => {
     console.log(`${fileName}`);
   });
 });
+
+const talksFiles = talks.Results.map(talk => {
+  const fileName = getTalkFileName(talk);
+  const eventString = `tccc${talk.EventId.split('/')[1]}`;
+  const speakerid = 'asdf';
+  const content = `---
+event: ${eventString}
+title: ${talk.Title}
+speaker: ${speakerid}
+layout: talk
+room: ${talk.Room}
+time: ${talk.Hour}
+---
+${talk.Abstract}`;
+
+  return { fileName, content, eventString };
+});
+
+talksFiles.forEach(async file => {
+  const { fileName, content, eventString } = file;
+  const folder = `out/events-content/_talks/${eventString}`;
+
+  await fs.ensureDir(folder);
+
+  fs.writeFileSync(`${folder}/${fileName}`, content);
+  console.log(fileName);
+});
+
+function getTalkFileName(talk) {
+  const originalTitle = talk.Title;
+  const author = talk.Author;
+  const titleWithoutSpaces = `${author
+    .split(' ')
+    .join('_')}_${originalTitle.split(' ').join('_')}`;
+  return `${titleWithoutSpaces.replace(/\W/g, '').toLowerCase()}.html`;
+}
